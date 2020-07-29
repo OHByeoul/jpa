b00c3bd6c9bed7cafa80307d9c38dea206986bce
오한별 <suriessay@naver.com>
2020-07-30 00:00:49 +0900

    spring data jpa test 작성

---
+++ src/test/java/com/study/jpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.study.jpa.repository;
+
+import com.study.jpa.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember(){
+        Member member = new Member("mem");
+        Member savedMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+
+    }
+
+}
